a6371e7132f546ae1ac0226b2543af50f5b36c34
Shashank Singhania <39347320+shashanksinghania@users.noreply.github.com>
2020-08-11 13:56:34 +0530

    InfoBox component created

---
+++ src/InfoBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+function InfoBox(props) {
+    return (
+        <Card className="infoBox">
+            <CardContent>
+                <Typography className="infoBox__title" color="textSecondary">
+                    {props.title}
+                </Typography>
+
+                <h2 className="infoBox__cases">
+                    {props.cases}
+                </h2>
+                <Typography className="infoBox__total" color="textSecondary">
+                    {props.total} Total
+            </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
